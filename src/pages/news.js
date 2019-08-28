9104721277d1289827d8ddc7a82cdfb8f9a1ca58
Guido <Guido@mac.local>
2019-08-28 13:45:41 +0200

    post page

--- src/pages/news.js
+++ src/pages/news.js
@@ -4,34 +4,50 @@ import Layout from "../components/layout"
 import Title from "../components/title"
 import { Helmet } from "react-helmet"
 
+
+
 export default ({ data }) => {
   return (
     <Layout>
       <Helmet>
         <title>Our Mission | VeggieMedCheeses</title>
       </Helmet>
-      <section className="container">
-        <Title title="NEWS" />
-
+      <Title title="NEWS" />
+      <section className="container blog-section" style={{marginTop: "40px", marginBottom: "180px"}}>
         {data.allWordpressPost.edges.map(({ node }) => (
-          <div style={{ marginBottom: "60px" }}>
+          <div style={{marginBottom: "60px"}}>
             <h3>{node.title}</h3>
-            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            <div className="blog-excerpt" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
             <Link to={node.slug}>
-            <p>> Read more</p>
-          </Link>
+              <p>> Read more</p>
+            </Link>
+            <div style={{borderBottom: "2px solid #f7f7f7", paddingTop: "20px"}}></div>
           </div>
         ))}
-
-        
       </section>
+      <style jsx="true">{`
+          .blog-section{
+              width: 95%;
+              
+          }
+          .blog-excerpt{
+              color: #777;
+              position: relative;
+              bottom: 10px
+          }
+          @media(min-width:1200px){
+            .blog-section{
+              width: 50%
+          }
+          }
+          `}</style>
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
   query {
-    allWordpressPost{
+    allWordpressPost {
       edges {
         node {
           title
@@ -42,23 +58,3 @@ export const pageQuery = graphql`
     }
   }
 `
-// export const pageQuery = graphql`
-//   query {
-//     allWordpressPost(sort: { fields: [date] }) {
-//       edges {
-//         node {
-//           title
-//           excerpt
-//           slug
-//         }
-//       }
-//     }
-//     allWordpressWpMedia {
-//         edges {
-//           node {
-//             source_url
-//           }
-//         }
-//       }
-//   }
-// `
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,13 +4,47 @@ import { graphql } from "gatsby"
 
 export default ({ data }) => {
   const post = data.allWordpressPost.edges[0].node
-  console.log(post)
+
   return (
     <Layout>
-      <div>
+    <div>
+          {post.featured_media ? (
+               <img className="blog-image" src={post.featured_media.source_url} style={{width: "100%", height:"auto"}}/>
+          ) : (
+             <div></div>
+          )}
+        <div className="blog-post">
         <h1>{post.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        </div>
       </div>
+      <style jsx="true">{`
+          .blog-post p{
+              color: #777;
+              margin: 0 10px
+          }
+          .blog-post h1{
+              text-align: center;
+              margin-top: 40px
+          }
+          .blog-image{
+                  max-width: 700px;
+                  display: block;
+                  margin: 0 auto;
+                  border-radius: 4px;
+                  box-shadow: rgba(12, 5, 5, 0.4) 0px 0px 20px 0px;
+                  
+              }
+          @media(min-width:1200px){
+              .blog-image{
+                  max-width: 700px;
+              }
+              .blog-post{
+                  width: 50%;
+                  margin: 0 auto
+              }
+          }
+          `}</style>
     </Layout>
   )
 }
@@ -21,6 +55,9 @@ export const query = graphql`
         node {
           title
           content
+          featured_media {
+            source_url
+          }
         }
       }
     }
