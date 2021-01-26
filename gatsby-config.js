27f96e28a1229f82168f0c66ab53f6214da86c80
Guido <carucciguido@gmail.com>
2021-01-26 10:20:11 +0000

    description changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,43 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Default Starter',
+    description: 'New vegetarian-friendly versions of mediterranean cheeses.',
+    author: '@gatsbyjs'
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: { 
-        name: `images`,
-        path: `${__dirname}/src/images/` }
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    
-    {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
-      },
+        name: 'images',
+        path: `${__dirname}/src/images/` 
+}
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.ico' // This path is relative to the root of the site.
+      }
     },
     {
-      resolve: `gatsby-source-wordpress`,
+      resolve: 'gatsby-source-wordpress',
       options: {
         /*
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
          */
-        baseUrl: `http://blog.veggiemedcheeses.com/`,
+        baseUrl: 'http://blog.veggiemedcheeses.com/',
         // The protocol. This can be http or https.
-        protocol: `http`,
+        protocol: 'http',
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
@@ -44,14 +45,14 @@ module.exports = {
         hostingWPCOM: false,
         // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
         // This feature is untested for sites hosted on WordPress.com
-        useACF: true,
-      },
+        useACF: true
+      }
     },
     {
       resolve: 'gatsby-plugin-mailchimp',
       options: {
-        endpoint: 'https://veggiemedcheeses.us4.list-manage.com/subscribe/post?u=b4c49b4080d8bbdf43682e3d8&amp;id=aa85edd348',
-      },
-    },
-  ],
+        endpoint: 'https://veggiemedcheeses.us4.list-manage.com/subscribe/post?u=b4c49b4080d8bbdf43682e3d8&amp;id=aa85edd348'
+      }
+    }
+  ]
 }
