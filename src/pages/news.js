1df9c50bd66d113c2f7dc65d39b9542225a4078e
Shubhom <shubhom.juneja@gmail.com>
2023-03-28 18:55:23 +0530

    fixed news page fixes

--- src/pages/news.js
+++ src/pages/news.js
@@ -23,7 +23,7 @@ export default ({ data }) => {
             <h3>{node.title}</h3>
             <div className='blog-excerpt' dangerouslySetInnerHTML={{ __html: node.excerpt }} />
             <Link to={`/${node.slug}`}>
-              <p>> Read more</p>
+              <p> {'> Read more'}</p>
             </Link>
             <div style={{ borderBottom: '2px solid #f7f7f7', paddingTop: '20px' }} />
           </div>
